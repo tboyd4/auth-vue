d3e57a9499f835d0b6a80f424705a0527ea31d12
tboyd4 <tboyd1898@gmail.com>
2020-04-21 13:23:44 -0500

    added automatic login from local storage

--- src/main.js
+++ src/main.js
@@ -3,10 +3,29 @@ import App from './App.vue'
 import router from './router'
 import store from './vuex/store'
 
+import axios from 'axios'
+
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  created() {
+    const userString = localStorage.getItem('user');
+    if (userString) {
+      const userData = JSON.parse(userString)
+      this.$store.commit('SET_USER_DATA', userData)
+    }
+    axios.interceptors.response.use(
+      response => response,
+      error => {
+        if (error.response.status === 401) {
+          this.$store.dispatch('login')
+        }
+        return Promise.reject(error)
+      }
+    )
+  },
   render: h => h(App)
 }).$mount('#app')
